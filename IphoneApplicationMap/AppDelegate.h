9f38e7e8b49d0276add530eb595d91e33abdc8ff
Vadim's Mac <vadimsmac@vadimss-imac.local>
2016-08-25 12:07:31 +0300

    Initial Commit

---
+++ IphoneApplicationMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IphoneApplicationMap
+//
+//  Created by Vadim's Mac on 8/25/16.
+//  Copyright © 2016 Vadim's Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IphoneApplicationMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IphoneApplicationMap
+//
+//  Created by Vadim's Mac on 8/25/16.
+//  Copyright © 2016 Vadim's Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
